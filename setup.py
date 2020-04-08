1609932b99d5bc898f0931e5d19f07a3b590f7c6
Clark Teeple <cbteeple@gmail.com>
2020-04-08 16:30:30 -0400

    update version number for dev version 0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sorotraj',
-    version='0.3',
+    version='0.4',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Generate trajectories for soft robots from a file',
