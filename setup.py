c1085043f0758f36809b714c4d46f7df0a7ad854
Clark Teeple <cbteeple@gmail.com>
2020-04-09 10:28:02 -0400

    increment version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sorotraj',
-    version='0.5',
+    version='0.6',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Generate trajectories for soft robots from a file',
