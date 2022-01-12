e2993343f8d5357a5ec779ec6412bcda05354f62
cbteeple <cbteeple@g.harvard.edu>
2022-01-12 17:39:11 -0500

    added test suite for built trajectory length and interpolator final times

--- examples/build_one_trajectory.py
+++ examples/build_one_trajectory.py
@@ -1,6 +1,7 @@
 import sorotraj
 
-file_to_use = 'traj_setup/setpoint_traj_demo'
+#file_to_use = 'traj_setup/setpoint_traj_demo.yaml'
+file_to_use = 'traj_setup/waveform_traj_demo.yaml'
 
 build = sorotraj.TrajBuilder()
 build.load_traj_def(file_to_use)
@@ -12,22 +13,24 @@ for key in traj:
 interp = sorotraj.Interpolator(traj)
 actuation_fn = interp.get_interp_function(
                 num_reps=1,
-                speed_factor=2.0,
+                speed_factor=1.0,
                 invert_direction=False)
 final_time = interp.get_final_time()
 print("Final Interpolation Time: %f"%(final_time))
 
 actuation_fn2 = interp.get_interp_function(
                 num_reps=1,
-                speed_factor=2.0,
+                speed_factor=1.0,
                 invert_direction=[1,3])
 
 cycle_fn = interp.get_cycle_function(
                 num_reps=1,
-                speed_factor=2.0,
+                speed_factor=1.0,
                 invert_direction=[1,3])
 
 print("Interpolation at 2.155")
+print(actuation_fn(4))
+print(actuation_fn2(4))
 print(actuation_fn(2.155))
 print(actuation_fn2(2.155))
 print(cycle_fn([0.5, 2.0, 7.0]))
--- test/test_build_traj.py
+++ test/test_build_traj.py
@@ -1,35 +1,127 @@
 import sorotraj
+import os
+import pytest
 
-file_to_use = '../examples/traj_setup/setpoint_traj_demo.yaml'
+folder_to_use = '../examples/traj_setup'
+
+@pytest.fixture(autouse=True)
+def setup_and_teardown():
+    print('\nFetching trajectories')
+    yield
+    print('\nFinished!')
 
 
 
-build = sorotraj.TrajBuilder()
-build.load_traj_def(file_to_use)
-traj = build.get_trajectory()
-for key in traj:
-	print(key)
-	print(traj[key])
+#@pytest.fixture(scope='session')
+#def get_trajectories():
+#    out_data = []
+#    for data in tests_to_do:
+#        builder = sorotraj.TrajBuilder()
+#        builder.load_traj_def(os.path.join(folder_to_use,data[0]))
 
-interp = sorotraj.Interpolator(traj)
-actuation_fn = interp.get_interp_function(
-                num_reps=1,
-                speed_factor=2.0,
-                invert_direction=False)
-final_time = interp.get_final_time()
-print("Final Interpolation Time: %f"%(final_time))
+    #     out_curr = [builder]
+    #     out_curr.extend(data[1:])
+    #     out_data.append(out_curr)
 
-actuation_fn2 = interp.get_interp_function(
-                num_reps=1,
-                speed_factor=2.0,
-                invert_direction=[1,3])
+    # return out_data
+    
 
-cycle_fn = interp.get_cycle_function(
-                num_reps=1,
-                speed_factor=2.0,
-                invert_direction=[1,3])
+# def test_traj_builder(get_trajectories):
+#     for data in get_trajectories:
+#         one_traj_builder(data[0], data[1])
 
-print("Interpolation at 2.155")
-print(actuation_fn(2.155))
-print(actuation_fn2(2.155))
-print(cycle_fn([0.5, 2.0, 7.0]))
+
+
+def pytest_generate_tests(metafunc):
+    idlist = []
+    argvalues = []
+    for scenario in metafunc.cls.scenarios:
+        idlist.append(scenario[0])
+        items = scenario[1].items()
+        argnames = [x[0] for x in items]
+        argvalues.append(([x[1] for x in items]))
+    metafunc.parametrize(argnames, argvalues, ids=idlist, scope="class")
+
+
+def get_traj_builder(filename):
+    traj_builder = sorotraj.TrajBuilder()
+    traj_builder.load_traj_def(os.path.join(folder_to_use,filename))
+    return traj_builder
+
+
+scenarios_build = [  ('setpoint_empty_prefix', {'filename':'setpoint_traj_demo_0.yaml', 'expected_len':8}),
+               ('setpoint_oneline_prefix', {'filename':'setpoint_traj_demo_1.yaml', 'expected_len':9}),
+               ('setpoint_twoline_prefix', {'filename':'setpoint_traj_demo.yaml', 'expected_len':10}),
+               ('interp_setpoint', {'filename':'interp_setpoint.yaml', 'expected_len':91}),
+               ('waveform_traj', {'filename':'waveform_traj_demo.yaml', 'expected_len':96}),
+               ]
+
+
+class TestTrajBuilder:
+    scenarios = scenarios_build
+
+    def test_traj_builder(self, filename, expected_len):
+        traj_builder = get_traj_builder(filename)
+        traj = traj_builder.get_trajectory()
+        traj_len =0
+        for key in traj:
+            traj_seg = traj[key]
+            if isinstance(traj_seg, list):
+                traj_len += (len(traj_seg))
+
+        print(traj_len, expected_len)
+        assert traj_len == expected_len
+
+
+
+
+scenarios_interp = [  ('setpoint_empty_prefix', {'filename':'setpoint_traj_demo_0.yaml', 'expected_time':8}),
+               ('setpoint_oneline_prefix', {'filename':'setpoint_traj_demo_1.yaml', 'expected_time':8}),
+               ('setpoint_twoline_prefix', {'filename':'setpoint_traj_demo.yaml', 'expected_time':9}),
+               ('interp_setpoint', {'filename':'interp_setpoint.yaml', 'expected_time':5}),
+               ('waveform_traj', {'filename':'waveform_traj_demo.yaml', 'expected_time':8}),
+               ]
+
+class TestInterpolator:
+    scenarios = scenarios_interp
+
+    def test_interpolator(self, filename, expected_time):
+        traj_builder = get_traj_builder(filename)
+        traj = traj_builder.get_trajectory()
+        interp = sorotraj.Interpolator(traj)
+        actuation_fn = interp.get_interp_function(
+                    num_reps=1,
+                    speed_factor=1.0,
+                    invert_direction=False)
+        final_time = interp.get_final_time()
+
+        assert final_time == expected_time
+
+
+
+#===================================================
+# TODO: implement tests for the actual interp function and cycle function.
+
+def notest_interpolator():
+    interp = sorotraj.Interpolator(traj)
+    actuation_fn = interp.get_interp_function(
+                    num_reps=1,
+                    speed_factor=2.0,
+                    invert_direction=False)
+    final_time = interp.get_final_time()
+    print("Final Interpolation Time: %f"%(final_time))
+
+    actuation_fn2 = interp.get_interp_function(
+                    num_reps=1,
+                    speed_factor=2.0,
+                    invert_direction=[1,3])
+
+    cycle_fn = interp.get_cycle_function(
+                    num_reps=1,
+                    speed_factor=2.0,
+                    invert_direction=[1,3])
+
+    print("Interpolation at 2.155")
+    print(actuation_fn(2.155))
+    print(actuation_fn2(2.155))
+    print(cycle_fn([0.5, 2.0, 7.0]))
